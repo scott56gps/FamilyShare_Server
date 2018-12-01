f57f659f478e528c0f7c430a21f5149e23d7e138
Scott Nicholes <scott93gps@gmail.com>
2018-11-30 23:33:00 -0700

    Added async and try catch statements

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ app.get('/', (request, response) => {
     response.send("Welcome to the App!  This is an example database querying app with the potential to become the production server")
 })
 
-app.get('/exampleQuery', (request, response) => {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM user');
-    response.send(result.rows)
-    client.release()
+app.get('/exampleQuery', async (request, response) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM user');
+        response.send(result.rows)
+        client.release()
+    } catch (err) {
+        console.error(err);
+        response.send("Error " + err);
+    }
 })
