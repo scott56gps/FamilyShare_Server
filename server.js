f50c8d5584edb3e5bbbc650905d7d503a4e9a313
Scott Nicholes <scott93gps@gmail.com>
2018-12-04 16:17:42 -0700

    Added debug console.log

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get('/available', async (request, response) => {
     try {
         const client = await pool.connect()
         const result = await client.query("SELECT * FROM ancestor WHERE user_id IS NULL")
+        console.log(result.rows)
         response.send(result.rows)
         client.release()
     } catch (err) {
