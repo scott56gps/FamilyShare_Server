d3689b2bf124ed9086758f71d213d1314f2e8d66
Scott Nicholes <scott93gps@gmail.com>
2019-02-17 04:22:52 -0700

    Doing it the express-ws way

--- server.js
+++ server.js
@@ -313,8 +313,8 @@ app.ws('/reserve', (ws, request) => {
         ws.send('Connection for reserve is opened');
     });
 
-    ws.on('message', upload.none(), async (request, response) => {
-        console.log('Message received in reserve', request);
+    ws.on('message', (message) => {
+        console.log('Message received in reserve', message);
         // var givenNames = request.body.givenNames
         // var surname = request.body.surname
         // var familySearchId = request.body.familySearchId
